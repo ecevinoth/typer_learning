b99a274a32546189f08f453dd926fbd7ffd618be
vinoth <ecevinoth@gmail.com>
2022-01-01 09:26:27 +0800

    simplifed the base version

--- tutorials/my_custom_scrip.py
+++ tutorials/my_custom_scrip.py
@@ -3,19 +3,16 @@ from typing import Optional
 
 app = typer.Typer()
 
+
 @app.command()
-def hello(name: Optional[str] = None):
-    if name is None:
-        typer.echo("Hello there!")
-    else: 
-        typer.echo(f"hello {name}!")
+def hello(name: Optional[str] = "World"):
+    typer.echo(f"Hello {name}!")
+
 
 @app.command()
 def bye(name: Optional[str] = None):
-    if name is None:
-        typer.echo("bye bye!")
-    else:
-        typer.echo(f"bye {name}")
+    typer.echo(f"bye {name}!")
+
 
 if __name__ == '__main__':
     app()
